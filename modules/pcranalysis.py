a6bd4a039a826c0ae91e24588a83cb3a1fcaf487
Jason Record <g23guy@gmail.com>
2025-10-04 07:00:38 -0600

    added module

---
+++ modules/pcranalysis.py
@@ -0,0 +1,41 @@
+'''
+Pacemaker Cluster Report Analysis Module
+Copyright (c) 2025 SUSE LLC
+
+Module of functions that look for common cluster issues and suggestions.
+'''
+##############################################################################
+#  Copyright (C) 2025 SUSE LLC
+##############################################################################
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; version 2 of the License.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+__author__        = 'Jason Record <jason.record@suse.com>, Raine Curtis <raine.curtis@suse.com>'
+__date_modified__ = '2025 Oct 01'
+__version__       = '0.0.1'
+
+# IMPORTS
+import sys
+import os
+import re
+from distutils.version import LooseVersion
+
+# CONSTANTS
+
+# COMMON ISSUES
+
+def analyze():
+    print("Analyzing Cluster Report\n")
+
+
